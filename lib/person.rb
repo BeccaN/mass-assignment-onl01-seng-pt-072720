19c5a478528a1749ad303c88e612965b92e9e8fd
Rebecca Congi <rncongi@gmail.com>
2020-08-27 23:15:43 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,8 @@
 class Person
   
+  attr_accessor 
+  
   def initialize(attributes)
-    attributes.each {|key, value| self.send
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end   
 end
